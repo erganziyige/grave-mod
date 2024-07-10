69a4304a36ab9587d2dbc0b78475819bb8a01075
root <root@DESKTOP-OO613P7>
2024-07-10 20:20:44 +0800

    Initial commit

---
+++ src/main/java/com/erganziyige/modid/GraveModClient.java
@@ -0,0 +1,10 @@
+package com.erganziyige.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GraveModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
